78535611b74df56ac85931cd7d2b7ce3c97514cc
shili <shili_wymail@163.com>
2020-11-10 08:44:41 +0800

    thread homework

---
+++ Week_04/Homework03.java
@@ -0,0 +1,263 @@
+package com.sl.java00.week04;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
+ * 异步运行一个方法，拿到这个方法的返回值后，退出主线程？
+ * 写出你的方法，越多越好，提交到github。
+ * <p>
+ * 一个简单的代码参考：
+ */
+public class Homework03 {
+
+    private static Integer resultWithMethod = new Integer(-1);
+
+    private static AtomicInteger atomicInteger = new AtomicInteger(-1);
+
+    public static void main(String[] args) {
+        
+        /*long start=System.currentTimeMillis();
+        // 在这里创建一个线程或线程池，
+        // 异步执行 下面方法
+        
+        int result = sum(); //这是得到的返回值
+        
+        // 确保  拿到result 并输出
+        System.out.println("异步计算结果为："+result);
+         
+        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");*/
+
+//        method1();
+//        method2();
+//        method3();
+//        method4();
+//        method5();
+//        method6();
+        method7();
+//        for(int i=0; i < 20; i++);
+        // 然后退出main线程
+    }
+
+    private static void method7() {
+        long start = System.currentTimeMillis();
+        int result = -1;
+        // 在这里创建一个线程或线程池，
+        // 异步执行 下面方法
+        Thread thread = new Thread(() -> {
+            atomicInteger.set(Integer.valueOf(sum()));
+        });
+        thread.start();
+
+
+        //这是得到的返回值
+//        result = atomicInteger.get();
+
+        // 确保  拿到result 并输出
+        System.out.println("异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //6. 利用LockSupport的park,unpark方法，将主线程暂停，由子线程恢复
+    private static void method6() {
+        long start = System.currentTimeMillis();
+        int result = -1;
+        Thread mainThread = Thread.currentThread();
+        // 在这里创建一个线程或线程池，异步执行下面方法
+        Thread thread = new Thread(() -> {
+            resultWithMethod = Integer.valueOf(sum());
+            LockSupport.unpark(mainThread);
+        });
+        thread.start();
+
+        LockSupport.park();
+//        LockSupport.parkUntil(resultWithMethod, 10);
+        //这是得到的返回值
+        result = resultWithMethod;
+
+        // 确保  拿到result 并输出
+        System.out.println("method6异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //5. 利用ReentrantReadWriteLock，writeLock()和readLock()互斥读写共享变量
+    private static void method5() {
+        long start = System.currentTimeMillis();
+        ReadWriteLock lock = new ReentrantReadWriteLock(true);
+        int result = -1;
+        // 在这里创建一个线程或线程池，异步执行下面方法
+        Thread thread = new Thread(() -> {
+            try {
+                lock.writeLock().lock();
+                resultWithMethod = sum();
+            } finally {
+                lock.writeLock().unlock();
+            }
+
+        });
+        thread.start();
+
+        try {
+            // 让主线程比子线程晚些获取锁
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            lock.readLock().lock();
+            result = resultWithMethod;
+        } finally {
+
+            lock.readLock().unlock();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method5异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //4. 利用ReentrantLock，lock与unlock搭配，互斥访问共享变量
+    private static void method4() {
+        long start = System.currentTimeMillis();
+        ReentrantLock lock = new ReentrantLock(true);
+        int result = -1;
+        // 在这里创建一个线程或线程池，异步执行下面方法
+        Thread thread = new Thread(() -> {
+            try {
+                lock.lock();
+                resultWithMethod = sum();
+            } finally {
+                lock.unlock();
+            }
+
+        });
+        thread.start();
+
+        try {
+            // 让主线程比子线程晚些获取锁
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            lock.lock();
+            //这是得到的返回值
+            result = resultWithMethod.intValue();
+        } finally {
+            lock.unlock();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method4异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //3. 利用synchronized锁，让主线程和子线程互斥访问共享变量
+    private static void method3() {
+        long start = System.currentTimeMillis();
+        int result = -1;
+
+        // 在这里创建一个线程或线程池，异步执行下面方法
+        Thread thread = new Thread(() -> {
+            synchronized (resultWithMethod) {
+                resultWithMethod = sum();
+//                resultWithMethod.notifyAll();
+            }
+        });
+        thread.start();
+
+        try {
+            // 让主线程比子线程晚些获取锁
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        synchronized (resultWithMethod) {
+//            try {
+//                resultWithMethod.wait();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            result = resultWithMethod;
+            //这是得到的返回值
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method3异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //2. 利用Callable接口可以线程执行的返回值
+    private static void method2() {
+        long start = System.currentTimeMillis();
+        int result = -1;
+        // 在这里创建一个线程或线程池，异步执行下面方法
+        ExecutorService threadPool = Executors.newFixedThreadPool(1);
+        Future<Integer> future = threadPool.submit(() -> {
+            return sum();
+        });
+
+        try {
+            //这是得到的返回值
+            result = future.get().intValue();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method2异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+        // 结束
+        threadPool.shutdown();
+    }
+
+    //1. 创建线程和静态变量，将计算结果存入静态变量
+    private static void method1() {
+        long start = System.currentTimeMillis();
+        int result = -1;
+        // 在这里创建一个线程或线程池，异步执行下面方法
+        Thread thread = new Thread(() -> {
+            resultWithMethod = Integer.valueOf(sum());
+        });
+        thread.start();
+        try {
+            thread.join();
+            //这是得到的返回值
+            result = resultWithMethod.intValue();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method1异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    private static int sum() {
+        return fibo(36);
+    }
+
+    private static int fibo(int a) {
+        if (a < 2)
+            return 1;
+        return fibo(a - 1) + fibo(a - 2);
+    }
+}
