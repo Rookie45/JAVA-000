30243bcfa0e5719c2290d961e68f4ea4d236e7c9
shili <shili_wymail@163.com>
2020-11-10 11:40:37 +0800

    finish lesson 7 homework 2

--- Week_04/Homework03.java
+++ Week_04/Homework03.java
@@ -1,9 +1,23 @@
 package com.sl.java00.week04;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.LockSupport;
 import java.util.concurrent.locks.ReadWriteLock;
@@ -21,48 +35,289 @@ public class Homework03 {
 
     private static Integer resultWithMethod = new Integer(-1);
 
-    private static AtomicInteger atomicInteger = new AtomicInteger(-1);
-
     public static void main(String[] args) {
-        
-        /*long start=System.currentTimeMillis();
-        // 在这里创建一个线程或线程池，
-        // 异步执行 下面方法
-        
-        int result = sum(); //这是得到的返回值
-        
-        // 确保  拿到result 并输出
-        System.out.println("异步计算结果为："+result);
-         
-        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");*/
 
-//        method1();
-//        method2();
-//        method3();
-//        method4();
-//        method5();
-//        method6();
+        method1();
+        method2();
+        method3();
+        method4();
+        method5();
+        method6();
         method7();
-//        for(int i=0; i < 20; i++);
-        // 然后退出main线程
+        method8();
+        method9();
+        method10();
+        method11();
+        method12();
+        method13();
+        method14();
+        method15();
+        method16();
+
     }
 
-    private static void method7() {
+    //16. 利用ConcurrentHashMap的CAS线程安全的读写
+    private static void method16() {
+        long start = System.currentTimeMillis();
+        final Map<String, Integer> count = new ConcurrentHashMap<>();
+        int result = -1;
+        // 在这里创建一个线程或线程池，异步执行 下面方法
+        Thread thread = new Thread(() -> {
+            Integer value = Integer.valueOf(sum());
+            count.put("result", value);
+        });
+        thread.start();
+
+        try {
+            // 预计子线程计算完成时间
+            Thread.sleep(78L);
+            //这是得到的返回值
+            result = count.get("result").intValue();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method16异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //15. CopyOnWriteArrayList线程安全的读写List
+    private static void method15() {
+        long start = System.currentTimeMillis();
+        List<Integer> safeCollection = new CopyOnWriteArrayList();
+        int result = -1;
+        // 在这里创建一个线程或线程池，异步执行 下面方法
+        Thread thread = new Thread(() -> {
+            safeCollection.add(Integer.valueOf(sum()));
+        });
+        thread.start();
+
+        try {
+            // 预计子线程计算完成时间
+            Thread.sleep(78L);
+            //这是得到的返回值
+            result = safeCollection.get(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method15异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //14. Collections.synchronizedList()强制将入参的集合对象的操作加上同步
+    private static void method14() {
+        long start = System.currentTimeMillis();
+        List<Integer> safeCollection = Collections.synchronizedList(new ArrayList<Integer>());
+        int result = -1;
+        // 在这里创建一个线程或线程池，异步执行 下面方法
+        Thread thread = new Thread(() -> {
+            safeCollection.add(Integer.valueOf(sum()));
+        });
+        thread.start();
+
+        try {
+            // 预计子线程计算完成时间
+            Thread.sleep(78L);
+            //这是得到的返回值
+            result = safeCollection.get(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method14异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //13. 线程安全的集合类Vector
+    private static void method13() {
+        long start = System.currentTimeMillis();
+        Vector<Integer> safeCollection = new Vector<>();
+        int result = -1;
+        // 在这里创建一个线程或线程池，异步执行 下面方法
+        Thread thread = new Thread(() -> {
+            safeCollection.add(Integer.valueOf(sum()));
+        });
+        thread.start();
+
+        try {
+            // 预计子线程计算完成时间
+            Thread.sleep(78L);
+            //这是得到的返回值
+            result = safeCollection.get(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method13异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //12. 利用CompletableFuture工具类，异步执行完，join等待执行结果
+    private static void method12() {
+        long start = System.currentTimeMillis();
+        // 在这里创建一个线程或线程池，异步执行 下面方法
+        CompletableFuture.runAsync(() -> {
+            resultWithMethod = sum();
+        }).join();
+        //这是得到的返回值
+        int result = resultWithMethod;
+
+        // 确保  拿到result 并输出
+        System.out.println("method12异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //11. 利用FutureTask类get()方法，获取子线程计算结果
+    private static void method11() {
         long start = System.currentTimeMillis();
         int result = -1;
-        // 在这里创建一个线程或线程池，
-        // 异步执行 下面方法
+        // 在这里创建一个线程或线程池，异步执行 下面方法
+        FutureTask<Integer> task = new FutureTask<Integer>(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return Integer.valueOf(sum());
+            }
+        });
+        new Thread(task).start();
+        //这是得到的返回值
+        try {
+            result = task.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method11异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //10. 利用CyclicBarrier的await()方法，让主线程等待子线程完成状态
+    private static void method10() {
+        long start = System.currentTimeMillis();
+        int result = -1;
+        // 在这里创建一个线程或线程池，异步执行下面方法
+        Thread thread = new Thread(() -> {
+            resultWithMethod = Integer.valueOf(sum());
+        });
+        CyclicBarrier barrier = new CyclicBarrier(1, thread);
+        thread.start();
+
+        try {
+            barrier.await();
+            //这是得到的返回值
+            result = resultWithMethod;
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method10异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //9. 利用CountDownLatch的countDown()和await()方法，让主线程等待子线程完成
+    private static void method9() {
+        long start = System.currentTimeMillis();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        int result = -1;
+        // 在这里创建一个线程或线程池，异步执行下面方法
+        Thread thread = new Thread(() -> {
+            resultWithMethod = Integer.valueOf(sum());
+            countDownLatch.countDown();
+
+        });
+        thread.start();
+
+        try {
+            countDownLatch.await();
+            //这是得到的返回值
+            result = resultWithMethod;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method9异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //8. 利用Semaphore为1时，具有独占性，形成多线程访问互斥
+    private static void method8() {
+        long start = System.currentTimeMillis();
+        int result = -1;
+        Semaphore semaphore = new Semaphore(1);
+        // 在这里创建一个线程或线程池，异步执行下面方法
+        Thread thread = new Thread(() -> {
+            try {
+                semaphore.acquire();
+                resultWithMethod = Integer.valueOf(sum());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+            }
+        });
+        thread.start();
+
+        try {
+            // 让主线程比子线程晚些获取锁
+            Thread.sleep(1L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            semaphore.acquire();
+            //这是得到的返回值
+            result = resultWithMethod;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("method8异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    //7. 利用原子类的特性，完成多线程间共享变量读写
+    private static void method7() {
+        long start = System.currentTimeMillis();
+        AtomicInteger atomicInteger = new AtomicInteger(-1);
+        int result = -1;
+        // 在这里创建一个线程或线程池，异步执行 下面方法
         Thread thread = new Thread(() -> {
             atomicInteger.set(Integer.valueOf(sum()));
         });
         thread.start();
 
+        try {
+            // 预计子线程计算完成时间
+            Thread.sleep(78L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         //这是得到的返回值
-//        result = atomicInteger.get();
+        result = atomicInteger.get();
 
         // 确保  拿到result 并输出
-        System.out.println("异步计算结果为：" + result);
+        System.out.println("method7异步计算结果为：" + result);
 
         System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
     }
@@ -203,7 +458,7 @@ public class Homework03 {
         System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
     }
 
-    //2. 利用Callable接口可以线程执行的返回值
+    //2. 利用Future和Callable接口可以线程执行的返回值
     private static void method2() {
         long start = System.currentTimeMillis();
         int result = -1;
