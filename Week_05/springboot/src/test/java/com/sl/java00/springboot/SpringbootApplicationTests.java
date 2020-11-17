ed48402c85a93a008005870de76ee68a379aac5b
Rookie45 <shili_wymail@163.com>
2020-11-17 21:55:49 +0800

    Update SpringbootApplicationTests.java

--- Week_05/springboot/src/test/java/com/sl/java00/springboot/SpringbootApplicationTests.java
+++ Week_05/springboot/src/test/java/com/sl/java00/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,114 @@
 package com.sl.java00.springboot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.sl.java00.springboot.homework.custom.autoconfig.Student;
+import com.sl.java00.springboot.homework.custom.jdbc.CustomJDBC;
+import com.sl.java00.springboot.homework.custom.jdbc.CustomBatchJDBC;
+import com.sl.java00.springboot.homework.custom.jdbc.CustomHikariConfig;
+import com.sl.java00.springboot.homework.custom.jdbc.HikariJDBC;
+import org.junit.Test;
 
-@SpringBootTest
-class SpringbootApplicationTests {
+import javax.sql.DataSource;
+import java.util.ArrayList;
 
-//    @Test
-//    void contextLoads() {
-//    }
+public class SpringbootApplicationTests {
 
+    @Test
+    public void jdbcInsertTest() {
+        Student student1 = new Student(1, "aaa");
+        CustomJDBC customJDBC = new CustomJDBC();
+        customJDBC.insert(student1);
+    }
+
+    @Test
+    public void jdbcUpdateTest() {
+        Student student1 = new Student(1, "bbb");
+        CustomJDBC customJDBC = new CustomJDBC();
+        customJDBC.update(student1);
+    }
+
+    @Test
+    public void jdbcSelectTest() {
+        CustomJDBC customJDBC = new CustomJDBC();
+        System.out.println(customJDBC.select(1));
+    }
+
+    @Test
+    public void jdbcSelectAllTest() {
+        CustomJDBC customJDBC = new CustomJDBC();
+        System.out.println(customJDBC.selectAll());
+    }
+
+    @Test
+    public void jdbcDeleteTest() {
+        CustomJDBC customJDBC = new CustomJDBC();
+        System.out.println(customJDBC.delete(1));
+    }
+
+    @Test
+    public void jdbcBatchInsertTest() {
+        Student student1 = new Student(1, "aaa");
+        Student student2 = new Student(2, "bbb");
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(student1);
+        list.add(student2);
+        CustomBatchJDBC batchJDBC = new CustomBatchJDBC();
+        batchJDBC.batchInsert(list);
+    }
+
+    @Test
+    public void jdbcBatchUpdateTest() {
+        Student student1 = new Student(1, "ccc");
+        Student student2 = new Student(2, "ddd");
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(student1);
+        list.add(student2);
+        CustomBatchJDBC batchJDBC = new CustomBatchJDBC();
+        batchJDBC.batchUpdate(list);
+    }
+
+    @Test
+    public void jdbcBatchDeleteTest() {
+        Student student1 = new Student(1, "ccc");
+        Student student2 = new Student(2, "ddd");
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(student1);
+        list.add(student2);
+        CustomBatchJDBC batchJDBC = new CustomBatchJDBC();
+        batchJDBC.batchDelete(list);
+    }
+
+    @Test
+    public void hikariInsertTest() {
+        Student student1 = new Student(2, "bbb");
+        DataSource dataSource = new CustomHikariConfig().dataSource();
+        HikariJDBC hikariJDBC = new HikariJDBC();
+        hikariJDBC.setDataSource(dataSource);
+        hikariJDBC.insert(student1);
+    }
+
+    @Test
+    public void hikariUpdateTest() {
+        Student student1 = new Student(2, "ccc");
+        DataSource dataSource = new CustomHikariConfig().dataSource();
+        HikariJDBC hikariJDBC = new HikariJDBC();
+        hikariJDBC.setDataSource(dataSource);
+        hikariJDBC.update(student1);
+    }
+
+    @Test
+    public void hikariSelectTest() {
+        DataSource dataSource = new CustomHikariConfig().dataSource();
+        HikariJDBC hikariJDBC = new HikariJDBC();
+        hikariJDBC.setDataSource(dataSource);
+        System.out.println(hikariJDBC.select(1));
+    }
+
+
+    @Test
+    public void hikariDeleteTest() {
+        DataSource dataSource = new CustomHikariConfig().dataSource();
+        HikariJDBC hikariJDBC = new HikariJDBC();
+        hikariJDBC.setDataSource(dataSource);
+        hikariJDBC.delete(2);
+    }
 }
