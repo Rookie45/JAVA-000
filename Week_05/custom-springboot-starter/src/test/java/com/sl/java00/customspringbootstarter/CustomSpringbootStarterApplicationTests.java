fd7fb3fdf03109673f037c00467b80b6aa193644
Rookie45 <shili_wymail@163.com>
2020-11-18 16:39:15 +0800

    Update CustomSpringbootStarterApplicationTests.java

--- Week_05/custom-springboot-starter/src/test/java/com/sl/java00/customspringbootstarter/CustomSpringbootStarterApplicationTests.java
+++ Week_05/custom-springboot-starter/src/test/java/com/sl/java00/customspringbootstarter/CustomSpringbootStarterApplicationTests.java
@@ -1,13 +1,13 @@
 package com.sl.java00.customspringbootstarter;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CustomSpringbootStarterApplicationTests {
+//@SpringBootTest
+//class CustomSpringbootStarterApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    //@Test
+    //void contextLoads() {
+    //}
 
-}
+//}
