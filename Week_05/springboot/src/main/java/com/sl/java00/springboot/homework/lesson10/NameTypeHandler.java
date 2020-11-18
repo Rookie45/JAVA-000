3b9c2934e2e43f21f7b5fb6babf3d8e7ab76eafa
Rookie45 <shili_wymail@163.com>
2020-11-18 18:40:30 +0800

    Create NameTypeHandler.java

---
+++ Week_05/springboot/src/main/java/com/sl/java00/springboot/homework/lesson10/NameTypeHandler.java
@@ -0,0 +1,36 @@
+package com.sl.java00.springboot.homework.lesson10.handler;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class NameTypeHandler extends BaseTypeHandler {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement preparedStatement, int i, Object o, JdbcType jdbcType) throws SQLException {
+        preparedStatement.setString(i, (String) o);
+    }
+
+    @Override
+    public Object getNullableResult(ResultSet resultSet, String s) throws SQLException {
+        return parseName(resultSet.getString(s));
+    }
+
+    @Override
+    public Object getNullableResult(ResultSet resultSet, int i) throws SQLException {
+        return parseName(resultSet.getString(i));
+    }
+
+    @Override
+    public Object getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
+        return parseName(callableStatement.getString(i));
+    }
+
+    private String parseName(String value) {
+        return "spring" + value;
+    }
+}
