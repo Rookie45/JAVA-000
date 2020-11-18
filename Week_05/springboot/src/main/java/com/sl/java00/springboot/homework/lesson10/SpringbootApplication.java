58974286da8f26a8e924e67241507049c9d2ebb9
Rookie45 <shili_wymail@163.com>
2020-11-18 18:43:22 +0800

    Update SpringbootApplication.java

--- Week_05/springboot/src/main/java/com/sl/java00/springboot/homework/lesson10/SpringbootApplication.java
+++ Week_05/springboot/src/main/java/com/sl/java00/springboot/homework/lesson10/SpringbootApplication.java
@@ -1,13 +1,36 @@
 package com.sl.java00.springboot.homework.lesson10;
 
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
+    
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        generateArtifacts();
+    }
 
+    private void generateArtifacts() throws Exception {
+        List<String> warnings = new ArrayList<>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(
+                this.getClass().getResourceAsStream("/generatorConfig.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
 }
