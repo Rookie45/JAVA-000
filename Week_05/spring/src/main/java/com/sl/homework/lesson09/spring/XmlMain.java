f6dfe1081fc68dc3b34084b85168c71ce53e6715
Rookie45 <shili_wymail@163.com>
2020-11-18 20:30:26 +0800

    Update XmlMain.java

--- Week_05/spring/src/main/java/com/sl/homework/lesson09/spring/XmlMain.java
+++ Week_05/spring/src/main/java/com/sl/homework/lesson09/spring/XmlMain.java
@@ -3,6 +3,15 @@ package com.sl.homework.lesson09.spring.xml;
 import com.sl.homework.lesson09.spring.bean.Student;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
+/**
+ * Creating new XML configuration extensions can be done by following these (relatively) simple steps:
+ *
+ * Authoring an XML schema to describe your custom element(s).
+ * Coding a custom NamespaceHandler implementation (this is an easy step, don’t worry).
+ * Coding one or more BeanDefinitionParser implementations (this is where the real work is done).
+ * Registering the above artifacts with Spring (this too is an easy step).
+ */
 public class XmlMain {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext =
