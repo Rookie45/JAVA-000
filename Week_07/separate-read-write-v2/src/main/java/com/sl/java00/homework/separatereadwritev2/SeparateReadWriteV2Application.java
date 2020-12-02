4765fdd7c719ec84493bddf8cc378f8af3abad15
Rookie45 <shili_wymail@163.com>
2020-12-02 21:33:04 +0800

    Update SeparateReadWriteV2Application.java

--- Week_07/separate-read-write-v2/src/main/java/com/sl/java00/homework/separatereadwritev2/SeparateReadWriteV2Application.java
+++ Week_07/separate-read-write-v2/src/main/java/com/sl/java00/homework/separatereadwritev2/SeparateReadWriteV2Application.java
@@ -1,9 +1,11 @@
 package com.sl.java00.homework.separatereadwritev2;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.sl.java00.homework.separatereadwritev2.mapper")
 public class SeparateReadWriteV2Application {
 
     public static void main(String[] args) {
