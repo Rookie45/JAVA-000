be86ffa0fc74d4868c3b8a162246fd63de8b5359
Rookie45 <shili_wymail@163.com>
2020-12-09 20:39:33 +0800

    Update OrderController.java

--- Week_08/distributed-transaction-application/hmily-tcc/src/main/java/com/sl/java00/homework/hmilytcc/controller/OrderController.java
+++ Week_08/distributed-transaction-application/hmily-tcc/src/main/java/com/sl/java00/homework/hmilytcc/controller/OrderController.java
@@ -1,17 +1,14 @@
-package com.sl.java00.homework.hmilytcc.controller;
+package com.sl.java00.homework.hmilytccorder.controller;
 
-import com.sl.java00.homework.hmilytcc.model.OrderModel;
-import com.sl.java00.homework.hmilytcc.service.OrderService;
+import com.sl.java00.homework.hmilytccorder.model.OrderModel;
+import com.sl.java00.homework.hmilytccorder.service.impl.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,13 +17,34 @@ import java.util.List;
 @Slf4j
 public class OrderController {
 
-    @Autowired
     private OrderService orderService;
 
-    @GetMapping("/test")
-    public ResponseEntity<?> findOne(@RequestParam(name = "id") Integer id) {
-        OrderModel test = orderService.getTest(id);
-        return new ResponseEntity<>(test, HttpStatus.OK);
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    /**
+     * {
+     * "id":10,
+     * "orderId":10,
+     * "orderSn":"bbbbb",
+     * "orderStatus":0,
+     * "productId":28,
+     * "productName":"goods1",
+     * "productSn":"zxcvasd",
+     * "productQuantity":1,
+     * "skuId":20,
+     * "skuCode":"20201208121212"
+     * }
+     * @param order order
+     * @return response
+     */
+    @PostMapping("/test")
+    public ResponseEntity<?> transactOrder(@RequestBody OrderModel order) {
+        log.info("[TestController][save] order request is {}", order);
+        Boolean result = orderService.orderTransaction(order);
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
     @GetMapping("/tests")
@@ -35,24 +53,5 @@ public class OrderController {
         return new ResponseEntity<>(tests, HttpStatus.OK);
     }
 
-    @PostMapping("/test")
-    public ResponseEntity<?> save(@RequestBody OrderModel test) {
-        log.info("[TestController][save] test model is {}", test);
-        int result = orderService.save(test);
-        return new ResponseEntity<>(result, HttpStatus.CREATED);
-    }
 
-
-    @PutMapping("/test")
-    public ResponseEntity<?> update(@RequestBody OrderModel test) {
-        log.info("[TestController][save] test model is {}", test);
-        int result = orderService.update(test);
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/test")
-    public ResponseEntity<?> deleteOne(@RequestParam(name = "id") Integer id) {
-        int result = orderService.delete(id);
-        return new ResponseEntity<>(result, HttpStatus.NO_CONTENT);
-    }
 }
