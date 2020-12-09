68d06041512a5c729cd00c9b45c8cb52fa9696f2
Rookie45 <shili_wymail@163.com>
2020-12-09 20:42:50 +0800

    Create HmilyTccCommonApplication.java

---
+++ Week_08/distributed-transaction-application/hmily-tcc/src/main/java/com/sl/java00/homework/hmilytcc/HmilyTccCommonApplication.java
@@ -0,0 +1,13 @@
+package com.sl.java00.homework.hmilytcccommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmilyTccCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmilyTccCommonApplication.class, args);
+    }
+
+}
