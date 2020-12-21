e3e97723b97ad09deabc5fc6ae9c17335c4e7f42
Rookie45 <shili_wymail@163.com>
2020-12-21 11:08:27 +0800

    Update Rpcfx.java

--- Week_09/rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/client/Rpcfx.java
+++ Week_09/rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/client/Rpcfx.java
@@ -7,14 +7,20 @@ import io.kimmking.rpcfx.client.proxy.JdkProxy;
 
 public final class Rpcfx {
 
+    private static HashMap<String, RpcfxProxy> proxyMap = new HashMap<>();
+
     static {
         ParserConfig.getGlobalInstance().addAccept("io.kimmking");
+        proxyMap.put("JDK", JdkProxy.getInstance());
+        proxyMap.put("ByteBuddy", ByteBuddyProxy.getInstance());
     }
 
     public static <T> T create(final Class<T> serviceClass, final String url) {
 
 
-        return JdkProxy.getInstance().createProxy(serviceClass, url);
+        //return JdkProxy.getInstance().createProxy(serviceClass, url);
+        
+        return proxyMap.get("ByteBuddy").createProxy(serviceClass, url);
 
         // 0. 替换动态代理 -> AOP
 //        return (T) Proxy.newProxyInstance(Rpcfx.class.getClassLoader(), new Class[]{serviceClass}, new RpcfxInvocationHandler(serviceClass, url));
