95617d7bce04a738d9b65f7cf0c39959df52d624
Rookie45 <shili_wymail@163.com>
2020-12-22 21:51:39 +0800

    Update and rename TransactionService.java to ConsumerService.java

---
+++ Week_09/hmily-tcc/hmily-tcc-order/src/main/java/com/sl/java00/homework/hmilytccorder/service/ConsumerService.java
@@ -0,0 +1,7 @@
+package com.sl.java00.homework.hmilytccconsumer.service;
+
+import com.sl.java00.homework.hmilytcccommon.dto.TransactionDTO;
+
+public interface ConsumerService {
+    boolean transactionUSD2RMB(TransactionDTO dto);
+}
