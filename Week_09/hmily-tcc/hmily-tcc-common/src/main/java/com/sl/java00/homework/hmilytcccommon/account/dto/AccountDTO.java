2221c422a13715fbbde32fae959408c37412819c
Rookie45 <shili_wymail@163.com>
2020-12-22 20:58:16 +0800

    Update AccountDTO.java

--- Week_09/hmily-tcc/hmily-tcc-common/src/main/java/com/sl/java00/homework/hmilytcccommon/account/dto/AccountDTO.java
+++ Week_09/hmily-tcc/hmily-tcc-common/src/main/java/com/sl/java00/homework/hmilytcccommon/account/dto/AccountDTO.java
@@ -1,4 +1,28 @@
-package com.sl.java00.homework.hmilytcccommon.account.dto;
+package com.sl.java00.homework.hmilytcccommon.dto;
 
-public class AccountDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionDTO implements Serializable, Cloneable {
+
+    private static final long serialVersionUID = 5182719963109090492L;
+
+    private Integer consumerUserId;
+    private Integer providerUserId;
+    private BigDecimal account;
+    private Integer type;
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
