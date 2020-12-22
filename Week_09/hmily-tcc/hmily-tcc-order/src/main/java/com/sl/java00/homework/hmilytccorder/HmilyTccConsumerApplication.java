b1f936bf23e64a35bbb721170ff42afb5cd43d55
Rookie45 <shili_wymail@163.com>
2020-12-22 21:49:36 +0800

    Update and rename HmilyTccOrderApplication.java to HmilyTccConsumerApplication.java

--- Week_09/hmily-tcc/hmily-tcc-order/src/main/java/com/sl/java00/homework/hmilytccorder/HmilyTccConsumerApplication.java
+++ Week_09/hmily-tcc/hmily-tcc-order/src/main/java/com/sl/java00/homework/hmilytccorder/HmilyTccConsumerApplication.java
@@ -1,18 +1,20 @@
-package com.sl.java00.homework.hmilytccorder;
+package com.sl.java00.homework.hmilytccconsumer;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+@PropertySource("classpath:dubbo-consumer.properties")
 @ImportResource({"classpath:spring-dubbo.xml"})
-@MapperScan("com.sl.java00.homework.hmilytccorder.mapper")
-public class HmilyTccOrderApplication {
+@MapperScan("com.sl.java00.homework.hmilytcccommon.mapper")
+public class HmilyTccConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HmilyTccOrderApplication.class, args);
+        SpringApplication.run(HmilyTccConsumerApplication.class, args);
     }
 
 }
