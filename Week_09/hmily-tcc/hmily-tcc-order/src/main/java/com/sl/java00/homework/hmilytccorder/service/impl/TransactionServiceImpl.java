f5192e07f1082d7b7b527e54c219442f8f82e2c0
Rookie45 <shili_wymail@163.com>
2020-12-22 21:52:04 +0800

    Update TransactionServiceImpl.java

--- Week_09/hmily-tcc/hmily-tcc-order/src/main/java/com/sl/java00/homework/hmilytccorder/service/impl/TransactionServiceImpl.java
+++ Week_09/hmily-tcc/hmily-tcc-order/src/main/java/com/sl/java00/homework/hmilytccorder/service/impl/TransactionServiceImpl.java
@@ -1,67 +1,111 @@
-package com.sl.java00.homework.hmilytccorder.service.impl;
+package com.sl.java00.homework.hmilytccconsumer.service.impl;
 
-import com.sl.java00.homework.hmilytcccommon.inventory.api.InventoryService;
-import com.sl.java00.homework.hmilytcccommon.inventory.dto.InventoryDTO;
-import com.sl.java00.homework.hmilytccorder.mapper.OrderMapper;
-import com.sl.java00.homework.hmilytccorder.model.OrderModel;
-import com.sl.java00.homework.hmilytccorder.model.OrderStatusEnum;
-import com.sl.java00.homework.hmilytccorder.service.TransactionService;
+import com.sl.java00.homework.hmilytcccommon.api.TransactionService;
+import com.sl.java00.homework.hmilytcccommon.dto.TransactionDTO;
+import com.sl.java00.homework.hmilytcccommon.mapper.AccountMapper;
+import com.sl.java00.homework.hmilytcccommon.mapper.FreezeMapper;
+import com.sl.java00.homework.hmilytcccommon.model.AccountModel;
+import com.sl.java00.homework.hmilytcccommon.model.FreezeModel;
+import com.sl.java00.homework.hmilytccconsumer.service.ConsumerService;
 import lombok.extern.slf4j.Slf4j;
 import org.dromara.hmily.annotation.HmilyTCC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Service
+import java.math.BigDecimal;
+
+import static com.sl.java00.homework.hmilytcccommon.model.BalanceType.RMB;
+import static com.sl.java00.homework.hmilytcccommon.model.BalanceType.USD;
+
+/**
+ * A 库为consumer，B 库为provider
+ */
 @Slf4j
-public class TransactionServiceImpl implements TransactionService {
+@Service
+public class ConsumerServiceImpl implements ConsumerService {
 
-    private final OrderMapper orderMapper;
+    //汇率
+    private final static BigDecimal Rate = new BigDecimal(7.0);
 
-    private final InventoryService inventoryService;
+    private FreezeMapper freezeMapper;
+
+    private AccountMapper accountMapper;
+
+//    @DubboReference
+    private TransactionService transactionService;
 
     @Autowired(required = false)
-    public TransactionServiceImpl(OrderMapper orderMapper, InventoryService inventoryService) {
-        this.orderMapper = orderMapper;
-        this.inventoryService = inventoryService;
+    public ConsumerServiceImpl(FreezeMapper freezeMapper, AccountMapper accountMapper, TransactionService transactionService) {
+        this.freezeMapper = freezeMapper;
+        this.accountMapper = accountMapper;
+        this.transactionService = transactionService;
     }
 
-    /**
-     * 在进行交易前，先检查资源是否符合要求，在进行资源锁定，成功或失败后更新状态和对应的
-     *
-     * @param order 订单实体
-     * @return transaction success or failed, true to success, otherwise failed.
-     */
-    @HmilyTCC(confirmMethod = "confirmOrder", cancelMethod = "cancelOrder")
-    public Boolean transaction(OrderModel order) {
-        log.info("[TransactionServiceImpl][transaction] begin to transact order.");
-        InventoryDTO inventoryDTO = inventoryService.findByProductId(order.getProductId());
-        if (inventoryDTO.getStock() < order.getProductQuantity()) {
-            throw new RuntimeException("库存不足");
+    @HmilyTCC(confirmMethod = "confirm", cancelMethod = "cancel")
+    public boolean transactionUSD2RMB(TransactionDTO dto) {
+
+        // 1. 查询A中的账户余额
+        AccountModel accountA = findByUserId(dto.getConsumerUserId());
+        BigDecimal balanceUSD = accountA.getBalanceUSD();
+
+        BigDecimal transactionUSD = dto.getAccount();
+        if (transactionUSD.compareTo(balanceUSD) > 0) {
+            log.error("[ConsumerServiceImpl][transactionUSD2RMB] account USD insufficient balance ");
+            throw new RuntimeException("insufficient balance");
         }
+        accountA.setBalanceUSD(balanceUSD.subtract(transactionUSD));
 
-        return updateOrderStatus(order, OrderStatusEnum.PAYING)
-                && inventoryService.decrease(buildInventoryDTO(order));
+        // 2. 冻结A中的1美元
+        FreezeModel freezeModel = new FreezeModel();
+        freezeModel.setAccountId(accountA.getId());
+        freezeModel.setFreezeAmount(transactionUSD);
+        freezeModel.setFreezeType(USD.getCode());
+        // 暂不处理返回false的情况
+        freezeAccount(freezeModel, accountA);
+
+        // 3. 冻结B中的7人民币
+        transactionService.freezeAccount(dto);
+
+        log.info("[ConsumerServiceImpl][transactionUSD2RMB] =========进行try操作完成================ ");
+        return true;
     }
 
-    private Boolean updateOrderStatus(OrderModel order, OrderStatusEnum orderStatus) {
-        order.setOrderStatus(orderStatus.getCode());
-        return orderMapper.update(order) > 0;
+    // 4. confirm时，对A增加7人民币，对B进行增加1美元，并清除冻结表
+    @Transactional
+    public void confirm(TransactionDTO dto) {
+        log.info("[TransactionServiceImpl][freezeAccount] begin confirm {}", dto);
+        AccountModel accountA = findByUserId(dto.getConsumerUserId());
+        BigDecimal balanceRMB = accountA.getBalanceRMB();
+        accountA.setBalanceRMB(balanceRMB.add(dto.getAccount().multiply(Rate)));
+        accountMapper.update(accountA);
+        transactionService.updateAccount(dto);
+        freezeMapper.deleteByAccountId(accountA.getId());
+        log.info("[ConsumerServiceImpl][confirm] =========进行confirm操作完成================ ");
     }
 
-    private InventoryDTO buildInventoryDTO(OrderModel order) {
-        InventoryDTO inventoryDTO = new InventoryDTO();
-        inventoryDTO.setProductId(order.getProductId());
-        inventoryDTO.setLockStock(order.getProductQuantity());
-        return inventoryDTO;
+    // 5. cancel时，对A增加1美元，对B进行增加7人民币，并清除冻结表
+    @Transactional
+    public void cancel(TransactionDTO dto) {
+        AccountModel accountA = findByUserId(dto.getConsumerUserId());
+        BigDecimal balanceUSD = accountA.getBalanceUSD();
+        accountA.setBalanceUSD(balanceUSD.add(dto.getAccount()));
+        accountMapper.update(accountA);
+        dto.setType(RMB.getCode());
+        dto.setAccount(dto.getAccount().multiply(Rate));
+        transactionService.updateAccount(dto);
+        freezeMapper.deleteByAccountId(accountA.getId());
+        log.info("[ConsumerServiceImpl][cancel] =========进行cancel操作完成================ ");
     }
 
-    public void confirmOrder(OrderModel order) {
-        updateOrderStatus(order, OrderStatusEnum.PAY_SUCCESS);
-        log.info("=========进行订单confirm操作完成================");
+    @Transactional
+    boolean freezeAccount(FreezeModel freeze, AccountModel account) {
+        log.info("[ConsumerServiceImpl][freezeAccount] freeze A account {}, {} ", freeze, account);
+        return 0 < freezeMapper.save(freeze)
+                && 0 < accountMapper.update(account);
     }
 
-    public void cancelOrder(OrderModel order) {
-        updateOrderStatus(order, OrderStatusEnum.PAY_FAIL);
-        log.info("=========进行订单cancel操作完成================");
+    private AccountModel findByUserId(Integer userId) {
+        return accountMapper.selectByUserId(userId);
     }
 }
