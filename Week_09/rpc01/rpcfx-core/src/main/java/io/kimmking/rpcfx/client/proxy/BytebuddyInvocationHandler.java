fa13868a50f4e802067fd7a20477166d5c8175bf
Rookie45 <shili_wymail@163.com>
2020-12-25 12:16:16 +0800

    Update BytebuddyInvocationHandler.java

--- Week_09/rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/client/proxy/BytebuddyInvocationHandler.java
+++ Week_09/rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/client/proxy/BytebuddyInvocationHandler.java
@@ -3,6 +3,8 @@ package io.kimmking.rpcfx.client.proxy;
 import com.alibaba.fastjson.JSON;
 import io.kimmking.rpcfx.api.RpcfxRequest;
 import io.kimmking.rpcfx.api.RpcfxResponse;
+import io.kimmking.rpcfx.client.transport.RpcfxHttpClient;
+import io.kimmking.rpcfx.client.transport.TransportWrapper;
 import io.kimmking.rpcfx.exception.RpcfxException;
 import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.implementation.bind.annotation.AllArguments;
@@ -33,59 +35,16 @@ public class BytebuddyInvocationHandler {
 
     @RuntimeType
     public Object invoke(@This Object proxy, @Origin Method method, @AllArguments @RuntimeType Object[] args) {
-        RpcfxRequest request = new RpcfxRequest();
-        request.setServiceClass(this.serviceClass);
-        request.setMethod(method.getName());
-        request.setParams(args);
 
         RpcfxResponse response = null;
         try {
-            response = post(request, url);
+
+            TransportWrapper transportWrapper = new TransportWrapper(serviceClass, url, new RpcfxHttpClient());
+            response = transportWrapper.post(method, args);
             return response.getResult();
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new RpcfxException(e);
         }
     }
 
-    // 使用httpclient
-    private RpcfxResponse post(RpcfxRequest req, String url) throws IOException {
-        String reqJson = JSON.toJSONString(req);
-        System.out.println("req json: " + reqJson);
-
-        RequestConfig config = RequestConfig.custom()
-                .setConnectTimeout(6000)
-                .setConnectionRequestTimeout(6000)
-                .setSocketTimeout(6000)
-                .build();
-
-        CloseableHttpResponse httpResponse = null;
-        HttpPost httpPost = null;
-        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setMaxConnPerRoute(2)
-                .setMaxConnTotal(10)
-                .setDefaultRequestConfig(config)
-                .build()){
-
-            httpPost = new HttpPost(url);
-            StringEntity entity = new StringEntity(reqJson);
-            httpPost.setEntity(entity);
-            httpPost.setHeader("Content-type", "application/json");
-            httpPost.addHeader("Accept-Charset", "utf-8");
-
-            httpResponse = httpClient.execute(httpPost);
-            String respJson = EntityUtils.toString(httpResponse.getEntity(), StandardCharsets.UTF_8);
-            return JSON.parseObject(respJson, RpcfxResponse.class);
-        } finally {
-            if (null != httpResponse) {
-                try {
-                    httpResponse.close();
-                } catch (IOException e) {
-                    log.error("[BytebuddyInvocationHandler][post] httpResponse close failed. ");
-                }
-            }
-            if (null != httpPost) {
-                httpPost.releaseConnection();
-            }
-        }
-    }
 }
