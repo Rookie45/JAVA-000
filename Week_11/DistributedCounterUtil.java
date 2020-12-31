24b27b0575403d696dc48642017a9a5d061f8998
Rookie45 <shili_wymail@163.com>
2020-12-31 18:48:03 +0800

    Create DistributedCounterUtil.java

---
+++ Week_11/DistributedCounterUtil.java
@@ -0,0 +1,31 @@
+package com.github.rookie45.distributedredis.util;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.sync.RedisCommands;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+public class DistributedCounterUtil {
+    private static RedisCommands<String, String> redisCommands;
+
+    static {
+        RedisURI redisURI = RedisURI.builder()
+                .withHost("localhost")
+                .withPort(6379)
+                .withTimeout(Duration.of(60, ChronoUnit.SECONDS))
+                .build();
+        redisCommands = RedisClient.create(redisURI).connect().sync();
+    }
+
+    public void setCounter(String key, long value) {
+        if (null == redisCommands.get(key)) {
+            redisCommands.set(key,String.valueOf(value));
+        }
+    }
+
+    public boolean decrease(String key) {
+        return 0 < redisCommands.decr(key);
+    }
+}
