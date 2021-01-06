b0e1daa66b40dcb304f26703df909d3cb45bb1d2
Rookie45 <shili_wymail@163.com>
2021-01-06 18:54:30 +0800

    Update DistributedCounterUtil.java

--- Week_11/DistributedCounterUtil.java
+++ Week_11/DistributedCounterUtil.java
@@ -2,12 +2,20 @@ package com.github.rookie45.distributedredis.util;
 
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import io.lettuce.core.ScriptOutputType;
 import io.lettuce.core.api.sync.RedisCommands;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class DistributedCounterUtil {
+
+    private static final String DECR_LUA = "if tonumber(redis.call('get',KEYS[1])) > 0 then " +
+            "redis.call('decr',KEYS[1]) return 1 else return 0 end;";
+
+    private static AtomicReference<String> LUA_DECR = new AtomicReference<>();
+
     private static RedisCommands<String, String> redisCommands;
 
     static {
@@ -21,13 +29,14 @@ public class DistributedCounterUtil {
 
     public void setCounter(String key, long value) {
         String currValue = redisCommands.get(key);
-        if (null == currValue || 0 > Integer.valueOf(currValue)) {
+        if (null == currValue || 1 > Integer.valueOf(currValue)) {
             redisCommands.set(key, String.valueOf(value));
         }
     }
 
     public boolean decrease(String key) {
-        return 0 < redisCommands.decr(key);
+        LUA_DECR.compareAndSet(null, redisCommands.scriptLoad(DECR_LUA));
+        return redisCommands.evalsha(LUA_DECR.get(), ScriptOutputType.BOOLEAN, key);
     }
 
 }
